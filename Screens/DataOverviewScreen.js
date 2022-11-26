c523cc67803d9006793294ff84637b6184c9dc24
Niightfall <s.dreesmann@gmx.net>
2022-11-26 14:48:37 +0100

    small change font

--- Screens/DataOverviewScreen.js
+++ Screens/DataOverviewScreen.js
@@ -15,6 +15,9 @@ const dataRecent7to14 = data.slice(data.length-14, data.length-7);
 const average7to14 = dataRecent7to14.reduce((a,b) => a+b, 0)/dataRecent7to14.length;
 const average7to14rounded = Math.floor(average7to14*100)/100;
 
+const difference2weeks = Math.floor((average7daysrounded-average7to14rounded)*100)/100
+const vorzeichen2weeks = difference2weeks > 0 ? '+' : '-';
+
 const dataRecent30 = data.slice(data.length-30, data.length);
 const average30days = dataRecent30.reduce((a,b) => a+b, 0)/dataRecent30.length;
 const average30daysrounded = Math.floor(average30days*100)/100;
@@ -25,10 +28,10 @@ const average30daysrounded = Math.floor(average30days*100)/100;
         <Text style={styles.text2}>Durchschnittsgewicht der ... </Text>
         <Text style={styles.text}>letzten 30 Tage: {"\t\t\t\t\t"} {average30daysrounded} kg</Text>
         <Text style={styles.text}>vorletzten 7 Tage: {"\t\t\t"} {average7to14rounded} kg</Text>
-        <Text style={styles.text}>letzten 7 Tage: {"\t\t\t\t\t\t\t"} {average7daysrounded} kg</Text>
+        <Text style={styles.text}>letzten 7 Tage: {"\t\t\t\t\t\t"} {average7daysrounded} kg</Text>
         <Text style={styles.text2}>{"\n"}Differenz der ... </Text>
-        <Text style={styles.text}>letzten 2 Wochen: {"\t\t\t\t\t\t\t"} 
-                                {average7daysrounded - average7daysrounded} kg</Text>
+        <Text style={styles.text}>letzten 2 Wochen: {"\t\t\t\t\t"} {vorzeichen2weeks} 
+                                {difference2weeks} kg</Text>
     </View>
     );
 }
